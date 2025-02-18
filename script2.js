be658cd91750f99cb166f27a7c37ac84720b0f0b
ikhsanalfaridzi <muhamadikhsanalfaridzi@smkwikrama.sch.id>
2025-02-18 13:30:00 +0700

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,7 @@
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    alert('Terima kasih telah menunjukkan minat untuk belajar lebih lanjut tentang bullying. Silakan kunjungi sumber daya kami untuk informasi lebih lanjut!');
+});
+
+document.getElementById('backButton').addEventListener('click', function() {
+    window.location.href = '';
+});
